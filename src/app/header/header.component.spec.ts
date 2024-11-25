77d2dd146eacbe27c8bab9f63c14f9a340af9036
aymene01 <aymene.bousbia@veepee.com>
2024-11-25 14:35:07 +0100

    fix: test configureTestingModule cfg

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,15 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 import { HeaderComponent } from './header.component';
+
+import { AuthService } from '../core/auth.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-    });
+      providers: [
+        AuthService, // Provide the AuthService used in the component
+        {
+          provide: FIREBASE_OPTIONS, // Provide mock Firebase configuration
+          useValue: {
+            apiKey: 'test-api-key',
+            authDomain: 'test-project.firebaseapp.com',
+            projectId: 'test-project',
+            storageBucket: 'test-project.appspot.com',
+            messagingSenderId: 'test-id',
+            appId: 'test-app-id',
+          },
+        },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,15 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 import { HomeComponent } from './home.component';
+
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent, HeaderComponent, FooterComponent],
+      providers: [
+        {
+          provide: FIREBASE_OPTIONS,
+          useValue: {
+            apiKey: 'test-api-key',
+            authDomain: 'test-project.firebaseapp.com',
+            projectId: 'test-project',
+            storageBucket: 'test-project.appspot.com',
+            messagingSenderId: 'test-id',
+            appId: 'test-app-id',
+          },
+        },
+      ],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-    });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
