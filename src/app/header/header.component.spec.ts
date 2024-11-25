e5ddf29fe2474df549a0399cd05086ff75aac501
aymene01 <70906904+aymene01@users.noreply.github.com>
2024-11-25 15:51:26 +0100

    fix(test): improve async setup for component tests using Firebase options (#2)
    * fix: test configureTestingModule cfg
    
    * chore: update CI configuration in workflows
    
    * chore: remove unused AuthService from HeaderComponent tests
    
    * test: update header and home component tests to use mock services

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -2,14 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 
+import { AuthService } from '../core/services/auth.service'; // Import AuthService
+import { UserService } from '../core/services/user.service'; // Import UserService
+
+class MockUserService {}
+
+class MockAuthService {}
+
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
-    });
+      providers: [
+        { provide: UserService, useClass: MockUserService },
+        { provide: AuthService, useClass: MockAuthService },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,15 +1,43 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 import { HomeComponent } from './home.component';
+
+import { AuthService } from '../core/services/auth.service'; // Import AuthService
+import { UserService } from '../core/services/user.service'; // Import UserService
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+
+class MockAuthService {}
+
+class MockUserService {}
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [HomeComponent, HeaderComponent, FooterComponent],
+      providers: [
+        {
+          provide: FIREBASE_OPTIONS,
+          useValue: {
+            apiKey: 'test-api-key',
+            authDomain: 'test-project.firebaseapp.com',
+            projectId: 'test-project',
+            storageBucket: 'test-project.appspot.com',
+            messagingSenderId: 'test-id',
+            appId: 'test-app-id',
+          },
+        },
+        { provide: AuthService, useClass: MockAuthService },
+        { provide: UserService, useClass: MockUserService },
+      ],
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-    });
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
