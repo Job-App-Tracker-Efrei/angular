68b0c89d083996b93a74b17de67ea8642f820541
Edofo <48387251+Edofo@users.noreply.github.com>
2024-11-25 16:22:19 +0100

    remove env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyC2mQ2A1m9JQMl4z7NPBdQeG4BpHHiA4Ms',
-    authDomain: 'job-tracker-efrei.firebaseapp.com',
-    projectId: 'job-tracker-efrei',
-    storageBucket: 'job-tracker-efrei.firebasestorage.app',
-    messagingSenderId: '922794878150',
-    appId: '1:922794878150:web:9a0688025235de934723c7',
-    measurementId: 'G-44S3Q5LP0P',
-  },
+  firebaseConfig: {},
 };
