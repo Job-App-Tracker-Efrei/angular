32e12f9fbb16ade69f0220af25a794fc6fb9548b
aymene01 <aymene.bousbia@veepee.com>
2024-11-26 16:12:48 +0100

    fix: add ToastrService provider to auth component tests

--- src/app/pages/auth/login/login.component.spec.ts
+++ src/app/pages/auth/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 import { AuthService } from '@core/services/auth.service';
 
@@ -15,7 +16,13 @@ describe('LoginComponent', () => {
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
       imports: [FormsModule, ReactiveFormsModule],
-      providers: [{ provide: AuthService, useClass: MockAuthService }],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService },
+        {
+          provide: ToastrService,
+          useValue: { error: () => {}, success: () => {} },
+        },
+      ],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
--- src/app/pages/auth/register/register.component.spec.ts
+++ src/app/pages/auth/register/register.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 import { AuthService } from '@core/services/auth.service';
 
@@ -15,7 +16,13 @@ describe('RegisterComponent', () => {
     TestBed.configureTestingModule({
       declarations: [RegisterComponent],
       imports: [FormsModule, ReactiveFormsModule],
-      providers: [{ provide: AuthService, useClass: MockAuthService }],
+      providers: [
+        { provide: AuthService, useClass: MockAuthService },
+        {
+          provide: ToastrService,
+          useValue: { error: () => {}, success: () => {} },
+        },
+      ],
     });
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
