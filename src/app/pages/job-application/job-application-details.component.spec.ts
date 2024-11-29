1f4fc641d20d187d25b7c8c6b2d2c80d24df4a73
Edofo <48387251+Edofo@users.noreply.github.com>
2024-11-29 11:27:05 +0100

    Update job-application-details.component.spec.ts

--- src/app/pages/job-application/job-application-details.component.spec.ts
+++ src/app/pages/job-application/job-application-details.component.spec.ts
@@ -1,8 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ToastrService } from 'ngx-toastr';
 
 import { JobApplicationService } from '@core/services/job-application.service';
+
+import { FooterComponent } from 'src/app/footer/footer.component';
+import { HeaderComponent } from 'src/app/header/header.component';
+import { JobApplicationStatus } from 'src/types/job-application.type';
 
 import { JobApplicationDetailsComponent } from './job-application-details.component';
 
@@ -18,8 +24,23 @@ describe('JobApplicationDetailsComponent', () => {
 
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [JobApplicationDetailsComponent],
+      declarations: [
+        JobApplicationDetailsComponent,
+        HeaderComponent,
+        FooterComponent,
+      ],
       providers: [
+        {
+          provide: FIREBASE_OPTIONS,
+          useValue: {
+            apiKey: 'test-api-key',
+            authDomain: 'test-project.firebaseapp.com',
+            projectId: 'test-project',
+            storageBucket: 'test-project.appspot.com',
+            messagingSenderId: 'test-id',
+            appId: 'test-app-id',
+          },
+        },
         {
           provide: ActivatedRoute,
           useValue: {
@@ -27,11 +48,21 @@ describe('JobApplicationDetailsComponent', () => {
           },
         },
         { provide: JobApplicationService, useValue: mockJobApplicationService },
+        { provide: ToastrService, useValue: {} },
       ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(JobApplicationDetailsComponent);
     component = fixture.componentInstance;
+    component.jobApplication = {
+      id: '123',
+      userId: '123',
+      status: JobApplicationStatus.pending,
+      company: 'Test Company',
+      position: 'Software Engineer',
+      date: new Date(),
+      lastUpdate: new Date(),
+    };
     fixture.detectChanges();
   });
 
